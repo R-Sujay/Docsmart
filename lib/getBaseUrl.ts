1a623c172c4f4e54fd8da382fa22632a8c70898a
Sujay Rajesh <sujay.rajesh08@gmail.com>
2024-07-27 01:45:07 -0700

    fixed base url

--- lib/getBaseUrl.ts
+++ lib/getBaseUrl.ts
@@ -1,3 +1,3 @@
-const getBaseUrl = () => (process.env.NODE_ENV === "development" ? "http://localhost:3000" : `https://${process.env.VERCEL_URL}`);
+const getBaseUrl = () => (process.env.NODE_ENV === "development" ? "http://localhost:3000" : `https://docsmart-rsujays-projects.vercel.app/`);
 
 export default getBaseUrl;
